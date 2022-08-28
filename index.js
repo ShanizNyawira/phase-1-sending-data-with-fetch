e4ba64b1083dfd59a7f8445028b47634bd8ae4e2
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-08-28 20:23:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName, email) {
+    var userData = {
+    name: userName,
+    email: email
+};
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+})
+    .then(response => response.json())
+    .then(data => {
+          console.log(data);
+    })
+.catch(error => {
+    document.querySelector('div').innerHTML = error;
+});
+}
